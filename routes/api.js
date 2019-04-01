6948bcbc00b8d49aa975fc08283566c3d1e901d8
dabutvin <butvinik@outlook.com>
2019-03-31 22:10:39 -0700

    update route to hold data in memory - api doesn't refresh like it used to

--- routes/api.js
+++ routes/api.js
@@ -1,30 +1,44 @@
+var express = require('express')
+var request = require('request')
+var router = express.Router()
+var data = []
 
-var express = require('express');
-var request = require('request');
-var router = express.Router();
+fetchData()
+setInterval(fetchData, 1000 * 60 * 10)
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
+  if (data.length) return res.send(data.pop())
+  fetchData(() => {
+    res.send(data.pop())
+  })
+})
 
-	request({ url: "https://venmo.com/api/v5/public?limit=1" }, function(error, response, body) {
-        if(error) {
-            console.log("Error: " + error);
-        } else {
-        	try {
-	            var result = JSON.parse(body);
-	            var payment = result.data[0];
-	            res.send(
-	            {
-	            	id: payment.payment_id,
-	            	message: payment.message,
-	            	picture: payment.actor.picture,
-	            	handle: payment.actor.username
-	            });
-	        } catch (ex) {
-	        	res.send(ex);
-	        }
+function fetchData(cb) {
+  request({ url: 'https://venmo.com/api/v5/public' }, function(
+    error,
+    response,
+    body
+  ) {
+    if (error) {
+      console.log('Error: ' + error)
+    } else {
+      try {
+        let result = JSON.parse(body)
+        for (let i = 0; i < result.data.length; i++) {
+          let payment = result.data[i]
+          data.push({
+            id: payment.payment_id,
+            message: payment.message,
+            picture: payment.actor.picture,
+            handle: payment.actor.username
+          })
         }
-    });
-});
+        if (cb) cb()
+      } catch (ex) {
+        console.log('Error: ' + ex)
+      }
+    }
+  })
+}
 
-module.exports = router;
+module.exports = router
